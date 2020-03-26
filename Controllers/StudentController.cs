9ef3b41a323f1decb75af58b557e1c0ecd60c699
AakashThapa <aakash.thapa@vicesoftware.com>
2020-03-26 20:23:13 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetWebApp.Controllers
+{
+    [Route("api/[Controller]")]
+    public class StudentController : Controller
+    {
+        StudentDAL obj = new StudentDAL();
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Students> Get()
+        {
+            return obj.GetAllStudents();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public Students Get(int id)
+        {
+            return obj.GetStudentData(id);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public int Post(Students student)
+        {
+            Console.WriteLine(student);
+            return obj.CreateStudent(student);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public int Put(Students student)
+        {
+            return obj.UpdateStudent(student);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public int Delete(int id)
+        {
+            return obj.DeleteStudent(id);
+        }
+    }
+}
---
+++ Models/StudentDAL.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetWebApp.Models
+{
+    public class StudentDAL
+    {
+        readonly StudentDBContext db = new StudentDBContext();
+
+        //this method will get all the student record
+        public IEnumerable<Students> GetAllStudents()
+        {
+            return db.Students.ToList();
+        }
+
+        //this method will add a new student 
+        public int CreateStudent(Students student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            return 1;
+
+        }
+        //this method will update the existing student record    
+        public int UpdateStudent(Students student)
+        {
+            db.Entry(student).State = EntityState.Modified;
+            db.SaveChanges();
+            return 1;
+        }
+        //this method will get detail of a specific student
+        public Students GetStudentData(int id)
+        {
+            Students student = db.Students.Find(id);
+            return student;
+        }
+        //this method will delete the specifig student record    
+        public int DeleteStudent(int id)
+        {
+            Students emp = db.Students.Find(id);
+            db.Students.Remove(emp);
+            db.SaveChanges();
+            return 1;
+        }
+
+    }
+}
